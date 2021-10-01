72743e567bf1bb42fe855f48b44fb125a5b284c1
root <root@DESKTOP-EUOO1RJ.localdomain>
2021-10-01 17:23:25 +0300

    fixed method for slices, removed redundant couting method

--- main.py
+++ main.py
@@ -1,90 +1,80 @@
-from math import floor
-
 class FizzBuzzGenerator:
     """
     Generator class for FizzBuzz game
 
-    start || The first number to start the count from;\n
-    fizz || Custom Fizz;\n
-    buzz || Custom Buzz.\n
+    start - The first number to start the count from;
+    fizz - Custom Fizz;
+    buzz - Custom Buzz.
     """
 
     def __init__(self, start, stop = None, step=1, fizz=3, buzz=5):
+
         self.start = start
-        if fizz:
-            self.iFizz = fizz
-        if buzz:
-            self.iBuzz = buzz
-        if stop:
-            self.stop = abs(start - stop)//step
-        self.iFizzBuzz = fizz*buzz
-        self.slider = self.get_rem(start)
-        self.order = self.get_order(start)
-        self.stock = []
-        self.try_check = 0
-        if stop:
-            self.step = step if start < stop else step*-1
+        self.stop = stop if stop else None
+        self.step = step
+        self.fizz = fizz
+        self.buzz = buzz    
+        self.fizz_buzz = fizz*buzz
+        self.first_check = False
+        self.current = self.start
     
 
     def __iter__(self):
+
         return self
 
 
     def __next__(self):
-        if self.slider >= self.iFizzBuzz:
-            self.slider = 0 if self.step == 1 else self.slider-self.iFizzBuzz
-            self.order += 1
-        self.slider += 1*self.step
-        if len(self.stock) < self.iFizzBuzz:
-            self.stock.append(self.check_fizz_buzz(self.slider))
-        if self.try_check == self.iFizzBuzz:
-            self.try_check = 0
-
-        self.try_check += 1
-        ans = self.stock[self.try_check-1]
-        if self.stop <= 0:
-            raise StopIteration
+        if self.first_check:
+            if self. stop:
+                if (self.step > 0  and self.current < self.stop) or (self.step < 0 and self.current > self.stop):
+                    pass
+                else:
+                    raise StopIteration
+            self.current += 1*self.step
         else:
-            self.stop -= 1
-        return ans+self.iFizzBuzz*self.order if isinstance(ans, int) else ans
-
+            self.first_check = True
+        
+        return self.get_fizz_buzz(self.current)
     
+
     def __getitem__(self, args):
         
         if isinstance(args, int):
-            return self.check_fizz_buzz(args+self.start-1)
+            return self.get_fizz_buzz(args+self.start)
         else:
-            tmp_strt = args.start if args.start > self.start else self.start
-            if args.step:
-                return FizzBuzzGenerator(tmp_strt, stop=args.stop, step=args.step)
+            if args.start:
+                tmp_strt = args.start + self.start
             else:
-                return FizzBuzzGenerator(tmp_strt, stop=args.stop)
+                tmp_strt = self.start
+            
+            if args.stop:
+                tmp_stop = args.stop
+                if self.stop:
+                    if tmp_stop > self.stop:
+                        tmp_stop = self.stop
+                if args.stop < 0 and self.stop == None:
+                    raise Exception("Can't subtract from infinity")
+            else:
+                tmp_stop = None
 
-
-    def get_rem(self, start):
-        """
-        Method for define remains of cycle
-
-        start || The first number to start the count from.
-        """
-        return (start%self.iFizzBuzz)-1
-
-
-    def get_order(self, start):
-        """
-        Method for define order of сycle
-
-        start || The first number to start the count from.
-        """
-        return floor(start/self.iFizzBuzz)
+            if args.step:
+                return FizzBuzzGenerator(tmp_strt, tmp_stop, step=args.step)
+            else:
+                return FizzBuzzGenerator(tmp_strt, stop=tmp_stop)
     
 
-    def check_fizz_buzz(self, num):
+    def get_fizz_buzz(self, num):
         """
-        Method for calculate actual FizzBuzzNumber
+        Method for calculate actual FizzBuzz number
 
         num || Numer for calculation
         """
-        return 'FizzBuzz' if num%self.iFizzBuzz==0 else \
-            'Fizz' if num%self.iFizz==0 else \
-                'Buzz' if num%self.iBuzz==0 else num
+        if num % self.fizz_buzz == 0:
+            return 'FizzBuzz'
+        elif num % self.buzz == 0:
+            return 'Buzz'
+        elif num % self.fizz == 0:
+            return 'Fizz'
+        else:
+            return num
